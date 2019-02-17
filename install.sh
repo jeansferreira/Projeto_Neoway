b13aacbab561c7342126e860e1262d5918f6830d
jeansferreira <jeansferreira@gmail.com>
2019-02-17 16:04:14 -0300

    inclusão do arquivo install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+go get github.com/jmoiron/sqlx
+go get github.com/lib/pq
+go get github.com/acroca
+go get github.com/go-delve
+go get github.com/jeansferreira
+go get github.com/jmiron
+go get github.com/jmoiron
+go get github.com/karrick
+go get github.com/lib
+go get github.com/nsf
+go get github.com/pkg
+go get github.com/ramya-rao-a
+go get github.com/rogpeppe
+go get github.com/sqs
+go get github.com/uudashr
